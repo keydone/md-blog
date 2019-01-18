d9f7dd45e93b5f5e77aff4e05f38cd929f3feff6
keydone <k754708625@gmail.com>
2019-01-18 22:51:13 +0800

    Mod_重构发布页面

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ const Router = require('koa-router');
 
 const router = new Router();
 const index = require('../controllers/articles');
-// const data = require('../mock/index');
 
 router
     .get('/', index.findAll, (ctx) => {
